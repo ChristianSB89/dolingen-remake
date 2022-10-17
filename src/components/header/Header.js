e9c348df14c56da52aa6e2804f5fe14535d38189
ChristianSB <christiansb.kodehode@gmail.com>
2022-10-17 12:58:39 +0200

    Started working on header

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderWrapper } from "./headerStyle";
+
+function Header() {
+  return (
+    <HeaderWrapper>
+      <h1>Header</h1>
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
